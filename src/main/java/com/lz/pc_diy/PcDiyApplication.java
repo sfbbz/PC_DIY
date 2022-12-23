2316a18fbf7efbfb5ebb9bda1f2cb2e0f537600b
Ricardo <lizheng_sfbbz@163.com>
2022-12-23 15:27:00 +0800

    Initial commit

---
+++ src/main/java/com/lz/pc_diy/PcDiyApplication.java
@@ -0,0 +1,13 @@
+package com.lz.pc_diy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcDiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcDiyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lz/pc_diy/PcDiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.pc_diy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcDiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
